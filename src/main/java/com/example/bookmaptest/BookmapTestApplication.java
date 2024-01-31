6d2e54b1489952d6f6a71f40f6049bd6144f220a
Teract10s <trs.leonov@gmail.com>
2024-01-31 12:05:44 +0200

    init

---
+++ src/main/java/com/example/bookmaptest/BookmapTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmaptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmapTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmapTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookmaptest/BookmapTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmaptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmapTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
